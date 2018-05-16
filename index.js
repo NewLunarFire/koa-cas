b2bbde5d7eca1e033f2cce5fcde9b397f9af4e72
Tommy Savaria <tommy.savaria@protonmail.ch>
2018-05-16 17:56:16 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const superagent = require('superagent');
+const xml2js = require("xml-js").xml2js;
+
+module.exports = function(options) {
+    return {
+        'redirect': async function(ctx, next) {
+            ctx.response.status = 302;
+            ctx.response.set('Location', options.server + '/login?service=' + encodeURIComponent(options.service));
+        },
+        'callback': async function(ctx, next) {
+            if(ctx.request.query.ticket) {
+                const uri = baseUrl + '/serviceValidate?ticket=' + ctx.request.query.ticket + '&service=' + encodeURIComponent(options.service);
+                
+                const res = await superagent
+                    .post(uri)
+                    .set("Accept-Language", 'en-us');
+                
+                const casResponse = xml2js(res.text, {compact: true});
+            
+                if(casResponse["cas:serviceResponse"] && casResponse["cas:serviceResponse"]["cas:authenticationSuccess"]) {
+                    ctx.cas.user = {};
+                    const attributes = casResponse["cas:serviceResponse"]["cas:authenticationSuccess"]["cas:attributes"];
+                    
+                    Object
+                        .keys(attributes)
+                        .forEach((key) => ctx.cas.user[key.substr(4)] = attributes[key]);
+                    
+                    await next();
+                } else {
+                    throw new Error("Error: Authentication failed");
+                }
+            } else {
+                throw new Error("Error: Missing ticket");
+            }
+        }
+    }
+}
